249054a0c165e7d576d6f78e1e75d001cc8e6d22
Dheeraj <dhirajpandey512@gmail.com>
2025-09-15 00:44:17 +0530

    feat(search): add className prop for customizable styling in Search component style(globals): rename code.css to code-block.css and clean up unused variables

--- src/components/ui/search.tsx
+++ src/components/ui/search.tsx
@@ -3,8 +3,9 @@
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 import Icon from './icon';
+import { cn } from '@/lib/utils';
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search({ className, placeholder }: { className?: string, placeholder: string }) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
@@ -22,18 +23,20 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }, 300);
 
     return (
-        <div className="flex items-center justify-start gap-2 border-2 rounded-md p-2">
-            <label htmlFor="search" className="sr-only">
-                {placeholder}
-            </label >
-            <Icon.search className="size-5 text-muted-foreground" />
-            <input
-                id="search"
-                className="text-sm focus:outline-none placeholder:text-muted-foreground"
-                placeholder={placeholder}
-                onChange={(e) => handleSearch(e.target.value)}
-                defaultValue={searchParams.get('query')?.toString()}
-            />
-        </div >
+        <div className={cn("flex flex-col justify-center", className)}>
+            <div className="flex items-center justify-start gap-2 border-2 rounded-md p-2">
+                <label htmlFor="search" className="sr-only">
+                    {placeholder}
+                </label>
+                <Icon.search className="size-5 text-muted-foreground" />
+                <input
+                    id="search"
+                    className="text-sm w-full focus:outline-none placeholder:text-muted-foreground"
+                    placeholder={placeholder}
+                    onChange={(e) => handleSearch(e.target.value)}
+                    defaultValue={searchParams.get('query')?.toString()}
+                />
+            </div>
+        </div>
     );
 }
